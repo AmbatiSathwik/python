5ffdc5c9f80097a41d1f4eb16e6d2b6f90cc70c2
Ambati Sathwik <75169261+AmbatiSathwik@users.noreply.github.com>
2021-05-12 19:16:04 +0530

    Update linear_regression.py

--- machinelearning/linear_regression.py
+++ machinelearning/linear_regression.py
@@ -3,20 +3,42 @@ import pandas as pd
 import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import pickle
+import matplotlib.pyplot as plt
 
-check = 0
 data = pd.read_csv('data.csv')
-data = data[['G1','G2','G3','studytime','failures']]
+data = data[['G1','G2','G3','studytime','failures','sex']]
     
 X = np.array(data.drop(['G3'], 1) )
 Y = np.array(data['G3'])
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1)    
-   
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1)
-linear = LinearRegression()
-linear.fit(x_train, y_train)
-accuracy = linear.score(x_test,y_test)
+  
+#training    
+#for _ in range(10):
+#    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.1)
+#    linear = LinearRegression()
+#    linear.fit(x_train, y_train)
+#    
+#    accuracy = linear.score(x_test,y_test)
+#    print(accuracy)
+#    if check < accuracy:
+#        check = accuracy
+#        with open('grade.pickle', 'wb') as fh:
+#            pickle.dump(linear, fh)
 
-predict = linear.predict(x_test)
-for i in range(len(predict)):
-    print(x_test[i],'         ', predict[i], '        ' ,y_test[i])
+            
+load = open('grade.pickle', 'rb')
+linear = pickle.load(load)
+predict = linear.predict(X)
+acc = linear.score(x_test,y_test)
+print(acc)
+
+p = 'G1'
+
+plt.scatter(data[p],predict)
+plt.xlabel(p)
+plt.ylabel('prediction')
+
+plt.scatter(data[p],data['G3'])
+plt.xlabel(p)
+plt.ylabel('Real One')
