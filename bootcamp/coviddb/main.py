2dcaab0186a1de62cb1df4e1031a03164bb2ae2c
Ambati Sathwik <75169261+AmbatiSathwik@users.noreply.github.com>
2021-06-10 21:13:23 +0530

    Update main.py

--- bootcamp/coviddb/main.py
+++ bootcamp/coviddb/main.py
@@ -8,13 +8,16 @@ app = Flask("covid cases")
 def hello():
     conc = pg.connect(database="covidcases")
     cursor = conc.cursor()
-    items = cursor.execute("select state,active_cases,deaths from covid_data")
+    cursor.execute("select state,active_cases,deaths from covid_data")
     cases = []
     for state, active_cases, deaths in cursor.fetchall():
-        item = f"{state}: <br/>  Active Cases:{active_cases} <br/> Deaths : {deaths}"
+        item = f"<a href=/find?state={state}>{state}<a/>: <br/>  Active Cases:{active_cases} <br/> Deaths : {deaths}"
         cases.append(item)
     l = "<hr/>".join(cases)
     return l
+
+
+items = []
 
 
 @app.route("/add")
@@ -22,3 +25,19 @@ def add_item():
     item = request.args.get("item")
     items.append(item)
     return f"No.of items : {len(items)}"
+
+
+@app.route("/find")
+def active_cases():
+    state = request.args.get("state")
+    conc = pg.connect(database="covidcases")
+    cursor = conc.cursor()
+    cursor.execute(
+        "select state,active_cases,deaths from covid_data where state like %s",
+        (state.title() + "%",),
+    )
+    for name, active_cases, deaths in cursor.fetchall():
+        a = active_cases
+        b = deaths
+        l = f"<h2>{name}:<h2/><h3>Active Cases : {a} <br/> Deaths : {b}<h3/>"
+        return l
