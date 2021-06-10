b8ec9067f8cb2cb7deecbef86a146fb41d14865e
Ambati Sathwik <75169261+AmbatiSathwik@users.noreply.github.com>
2021-06-10 20:09:24 +0530

    Update covid.py

--- bootcamp/coviddb/covid.py
+++ bootcamp/coviddb/covid.py
@@ -9,14 +9,24 @@ states = soup.find_all("label")
 fh = open("covid_data.csv", "w")
 
 j = 0
-for i in states:
+for i in states[:-2]:
     fh.write(i.get_text())
     fh.write(",")
     for k in range(4):
         try:
-            numbers = cases[j].get_text().split(" ")
-            for h in numbers:
-                fh.write(h)
+
+            if k != 1:
+                numbers = cases[j].get_text().split(" ")
+                for h in numbers:
+                    if h == "":
+                        fh.write("0")
+                        fh.write(",")
+                    else:
+                        fh.write(h)
+                        fh.write(",")
+            else:
+                element = cases[j].get_text().split(" ")
+                fh.write(element[0])
                 fh.write(",")
             j += 1
         except:
